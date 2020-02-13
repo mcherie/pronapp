3dffd23c4f5045d1f411abbfd37970a807528c03
Cher <red_phoenix417@yahoo.com>
2020-02-13 11:19:40 -0500

    Created main page with title and desc

---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Main = () => {
+  return (
+    <div style={{backgroundColor: "#6f00ff", height: "25rem", top: 0, position: "fixed"}}>
+      <p style={{color:"white", fontSize: "2rem", fontWeight: "bold"}}>Premium Tests</p>
+      <p style={{paddingLeft: "7rem", paddingRight: "7rem", color:"white",}}>These premium speaking tests will help you to prepare for the speaking portion of the IELTS examination. </p>
+    </div>
+  )
+}
